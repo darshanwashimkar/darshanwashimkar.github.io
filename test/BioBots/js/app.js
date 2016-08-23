8559f654dc137dd6f863b9ce538694fb0e681215
darshan <darshan.wash@gmail.com>
2016-08-23 16:31:18 -0600

    reading json

---
+++ test/BioBots/js/app.js
@@ -0,0 +1,19 @@
+var jqxhr = $.getJSON( "../bioprint-data.json", function() {
+  console.log( "success" );
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+ 
+// Perform other work here ...
+ 
+// Set another completion function for the request above
+jqxhr.complete(function() {
+  console.log( "second complete" );
+});
