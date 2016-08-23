32178a2fa430d61855144bf5edbd1531ca7cc540
darshan <darshan.wash@gmail.com>
2016-08-23 17:27:33 -0600

    continue...

--- test/BioBots/js/app.js
+++ test/BioBots/js/app.js
@@ -1,20 +1,7 @@
 var jqxhr = $.getJSON( "http://darshanwashimkar.github.io/test/BioBots/bioprint-data.json", function() {
   console.log( "success" );
-})
-  .done(function() {
-    console.log( "second success" );
-  })
+})  
   .fail(function() {
-    alert("Hello! I am an alert box!!");
+    alert("Unable to read JSON file"");
     console.log( "error" );
-  })
-  .always(function() {
-    console.log( "complete" );
   });
- 
-// Perform other work here ...
- 
-// Set another completion function for the request above
-jqxhr.done(function() {
-  console.log( "second complete" );
-});
