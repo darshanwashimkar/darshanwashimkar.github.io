f87a4f23dccfe037612f2e72e69515ddb87675b4
darshan <darshan.wash@gmail.com>
2016-08-24 20:34:47 -0600

    Starting new...

--- test/BioBots/js/app.js
+++ test/BioBots/js/app.js
@@ -1,7 +1,44 @@
-var jqxhr = $.getJSON( "http://darshanwashimkar.github.io/test/BioBots/bioprint-data.json", function() {
+var jsonData = $.getJSON( "http://darshanwashimkar.github.io/test/BioBots/bioprint-data.json",{
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  },function() {
   console.log( "success" );
 })  
   .fail(function() {
-    alert("Unable to read JSON file"");
+    alert("Unable to read JSON file");
     console.log( "error" );
   });
+
+
+jsonData.done(function(data) {  
+  $.each(data, function(i,eachPrint){
+      if(eachPrint['print_info']['crosslinking']['cl_enabled'] == false){
+        cl = 'class="warning"';
+      }
+      else{
+        cl = '';
+      }
+      $('#data_presenter > tbody:last-child').append(                
+        '<tr>'+
+          '<td>'+eachPrint['user_info']['serial']+'</td>' +
+          '<td>'+eachPrint['user_info']['email']+'</td>' +
+          '<td '+cl+'>'+eachPrint['print_info']['crosslinking']['cl_duration']+'</td>' +
+          '<td '+cl+'>'+eachPrint['print_info']['crosslinking']['cl_intensity']+'</td>' +
+          '<td>'+eachPrint['print_info']['files']['input']+'</td>' +
+          '<td>'+eachPrint['print_info']['files']['output']+'</td>' +
+          '<td>'+eachPrint['print_info']['pressure']['extruder1']+'</td>' +
+          '<td>'+eachPrint['print_info']['pressure']['extruder2']+'</td>' +
+          '<td>'+eachPrint['print_info']['resolution']['layerHeight']+'</td>' +
+          '<td>'+eachPrint['print_info']['resolution']['layerNum']+'</td>' +
+          '<td>'+eachPrint['print_info']['wellplate']+'</td>' +
+          '<td>'+eachPrint['print_data']['livePercent']+'</td>' +          
+          '<td>'+eachPrint['print_data']['elasticity']+'</td>' +
+          '<td>'+eachPrint['print_data']['deadPercent']+'</td>' +
+        '</tr>'
+      );      
+      if(i == 100){
+        return(false);
+      }
+   });  
+});
